eb25e1b70743c06c32e841be190f19b55b19280f
Suraj <Suraj@Suraj>
2025-08-24 16:10:04 +0530

    Second commit

--- springmvcsearch/src/main/java/springmvcsearch/SearchController.java
+++ springmvcsearch/src/main/java/springmvcsearch/SearchController.java
@@ -1,16 +1,12 @@
 package springmvcsearch;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
